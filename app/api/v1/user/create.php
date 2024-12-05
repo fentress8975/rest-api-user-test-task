b98b09921a1a5ef2e703407c4a00749f94e85411
generallegion <generallegion@yandex.ru>
2024-12-05 05:02:20 +0400

    create

--- app/api/v1/user/create.php
+++ app/api/v1/user/create.php
@@ -1,41 +1,42 @@
 <?php
 
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: access");
+header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Allow-Credentials: true");
-header("Content-Type: application/json");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-echo "create";
+include_once "php/database.php";
+include_once "api/v1/objects/user.php";
+
 
 $database = new Database();
 $db = $database->getConnection();
 
 $user = new User($db);
 
-$data = json_decode(file_get_contents("php://input"));
+$data = json_decode(file_get_contents('php://input'), true);
+
 
 if (
-    !empty($data->login) &&
-    !empty($data->password) &&
-    !empty($data->description)
+    !empty($data['login']) &&
+    !empty($data['password']) &&
+    !empty($data['description'])
 ) {
-    $user->login = $data->name;
-    $user->password = $data->price;
-    $user->description = $data->description;
+    $user->login = $data['login'];
+    $user->password = $data['password'];
+    $user->description = $data['description'];
 
     if ($user->create()) {
         http_response_code(201);
 
         echo json_encode(array("message" => "Пользователь создан"));
-    }
-    else {
+    } else {
         http_response_code(503);
 
         echo json_encode(array("message" => "Невозможно создать пользователя"));
     }
-}
-else {
+} else {
     http_response_code(400);
 
     echo json_encode(array("message" => "Невозможно создать пользователя. Нету необходимых данных"));
